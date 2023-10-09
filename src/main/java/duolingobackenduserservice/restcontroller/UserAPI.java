7c637cf2b7bc9f03579f9b81845a180ee38838cb
Admin <phuckhoa33>
2023-10-09 23:59:15 +0700

    complete authentication

--- src/main/java/duolingobackenduserservice/restcontroller/UserAPI.java
+++ src/main/java/duolingobackenduserservice/restcontroller/UserAPI.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("*")
 @RequestMapping("/api/user")
 public class UserAPI {
 
